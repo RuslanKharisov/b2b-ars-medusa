d69111cca594f264b49f9f0c8d88c074471f943e
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-06-17 22:34:00 +0300

    front:yandex metrika

--- ars-storefront/src/app/layout.tsx
+++ ars-storefront/src/app/layout.tsx
@@ -3,17 +3,20 @@ import { Toaster } from "@medusajs/ui"
 import { GeistSans } from "geist/font/sans"
 import { Metadata } from "next"
 import "@/styles/globals.css"
+import YandexMetrikaContainer from "@/modules/common/components/yandex-metrika"
 
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
 }
 
 export default function RootLayout(props: { children: React.ReactNode }) {
+  const analyticsEnabled = !!(process.env.NODE_ENV === "production")
   return (
     <html lang="ru" data-mode="light" className={GeistSans.variable}>
       <body>
         <main className="relative">{props.children}</main>
         <Toaster className="z-[99999]" position="bottom-left" />
+        <YandexMetrikaContainer enabled={analyticsEnabled} />
       </body>
     </html>
   )
