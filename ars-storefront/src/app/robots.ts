fcb83d0e3e34febc8d09807ea29c932b84cfb6a1
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-06-20 14:13:36 +0300

    front: fix: robots ts & sitemap.ts  static

--- ars-storefront/src/app/robots.ts
+++ ars-storefront/src/app/robots.ts
@@ -2,13 +2,12 @@ import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: [
-            {
-                userAgent: '*',
-                allow: ['/ru/', '/ru/products/', '/ru/collections/', '/ru/categories/', '/ru/store'],
-                disallow: ['/ru/account', '/ru/checkout', '/ru/order', '/api/', '/admin/', '/private/'],
-            }
-        ],
+        rules:
+        {
+            userAgent: '*',
+            allow: ['/ru/', '/ru/products/', '/ru/collections/', '/ru/categories/', '/ru/store'],
+            disallow: ['/ru/account', '/ru/checkout', '/ru/order', '/api/', '/admin/', '/private/'],
+        },
         sitemap: 'https://arselectronic.ru/sitemap.xml',
         host: 'https://arselectronic.ru',
     }
