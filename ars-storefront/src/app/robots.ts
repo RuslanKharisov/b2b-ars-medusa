e847c253a810aaff07ce98f1f9707e6ddfd79a68
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-06-20 11:15:57 +0300

    front: dinamic robots  and sitemap

--- ars-storefront/src/app/robots.ts
+++ ars-storefront/src/app/robots.ts
@@ -1,12 +1,47 @@
 import type { MetadataRoute } from 'next'
+import { listRegions } from "@/lib/data/regions";
 
-export default function robots(): MetadataRoute.Robots {
+export default async function robots(): Promise<MetadataRoute.Robots> {
+    const baseUrl = 'https://arselectronic.ru';
+    const regions = await listRegions();
+    const defaultCountryCode = regions?.[0]?.countries?.[0]?.iso_2 || "ru";
+
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: '/private/',
-        },
-        sitemap: 'https://arselectronic.ru/sitemap.xml',
-    }
+        rules: [
+            {
+                userAgent: '*',
+                allow: [
+                    `/${defaultCountryCode}`,
+                    `/${defaultCountryCode}/products/`,
+                    `/${defaultCountryCode}/collections/`,
+                    `/${defaultCountryCode}/categories/`,
+                    `/${defaultCountryCode}/store`
+                ],
+                disallow: [
+                    `/${defaultCountryCode}/account`,
+                    `/${defaultCountryCode}/checkout`,
+                    `/${defaultCountryCode}/order`,
+                    '/api',
+                    '/admin',
+                    '/private'
+                ],
+            },
+            {
+                userAgent: 'YandexBot',
+                allow: [
+                    `/${defaultCountryCode}`,
+                    `/${defaultCountryCode}/products/`,
+                    `/${defaultCountryCode}/collections/`,
+                    `/${defaultCountryCode}/categories/`
+                ],
+                disallow: [
+                    `/${defaultCountryCode}/cart`,
+                    `/${defaultCountryCode}/checkout`
+                ],
+                crawlDelay: 1,
+            }
+        ],
+        sitemap: `${baseUrl}/sitemap.xml`,
+        host: baseUrl,
+    };
 }
